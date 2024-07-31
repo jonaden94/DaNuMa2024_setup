6cb7ce5b216546a599e79c6919e52e23ef61ecc5
GOESTERN-0886446 <u12112@gwdu102.global.gwdg.cluster>
2024-07-31 13:11:57 +0200

    hi

--- files/install_miniforge.sh
+++ files/install_miniforge.sh
@@ -1,8 +1,8 @@
 wget -O Miniforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
-bash Miniforge3.sh -b -p "conda"
+bash Miniforge3.sh -b -p "/scratch1/users/$USER/conda"
 rm Miniforge3.sh
 
 # to initially init
-bash conda/etc/profile.d/conda.sh
-bash conda/etc/profile.d/mamba.sh
-conda init
+bash /scratch1/users/$USER/conda/etc/profile.d/conda.sh
+bash /scratch1/users/$USER/conda/etc/profile.d/mamba.sh
+/scratch1/users/$USER/conda/bin/conda init
--- setup.sh
+++ setup.sh
@@ -1,2 +1,30 @@
-git clone 
-cp -f setup_summer_school/files/.bashrc .bashrc
+# what was done before running this file:
+# 1. create folder named "repos" in ~
+# 2. git clone https://github.com/jonaden94/setup_summer_school 
+# 3. cd repos/setup_summer_school
+
+# nice bashrc
+cp -f files/.bashrc .bashrc
+
+# clone repos
+git clone https://github.com/jonaden94/DaNuMa2024
+git clone https://github.com/jonaden94/PigDetect
+
+# symbolic link data
+ln -s /scratch1/projects/scc_gwdg_danuma_ss2024/data 
+
+# conda environment
+bash /user/henrich1/u12112/repos/setup_summer_school/files/install_miniforge.sh
+
+# run new bash
+exec bash -l
+
+# environment setup
+cd ..
+git clone https://github.com/jonaden94/exercises_summer_school
+cd exercises_summer_school
+source setup/setup.sh
+cd ..
+
+# install kernel for notebook
+python -m ipykernel install --user --name exenv --display-name "exenv"
