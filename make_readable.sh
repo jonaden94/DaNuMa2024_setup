aefd58c84827d5251fe81d2575a020e6ed2f17ae
GOESTERN-0886446 <u12112@gwdu102.global.gwdg.cluster>
2024-08-07 15:08:39 +0200

    update

---
+++ make_readable.sh
@@ -0,0 +1,9 @@
+# make files readable
+chmod -R a+r /scratch1/projects/scc_gwdg_danuma_ss2024/DaNuMa2024_setup
+# permit navigation into folders (i.e. make folder executable)
+find /scratch1/projects/scc_gwdg_danuma_ss2024/DaNuMa2024_setup -type d -exec chmod a+rx {} \;
+
+# make files readable
+chmod -R a+r /scratch1/projects/scc_gwdg_danuma_ss2024/raw_data
+# permit navigation into folders (i.e. make folder executable)
+find /scratch1/projects/scc_gwdg_danuma_ss2024/raw_data -type d -exec chmod a+rx {} \;
--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
-# what was done before running this file:
-# 1. create folder named "repos" in ~
-# 2. git clone https://github.com/jonaden94/DaNuMa2024_setup 
-
 # cd to home directory
 cd $HOME
 
+# make repo directory
+mkdir repos
+
 # nice bashrc
-cp -f $HOME/repos/DaNuMa2024_setup/setup_files/.bashrc $HOME/.bashrc
+cp -f /scratch1/projects/scc_gwdg_danuma_ss2024/DaNuMa2024_setup/setup_files/.bashrc $HOME/.bashrc
 
 # clone repos
 cd repos
@@ -21,15 +20,15 @@ mkdir /scratch1/users/$USER/output_data
 ln -s /scratch1/users/$USER/output_data data
 
 # conda environment
-bash $HOME/repos/DaNuMa2024_setup/setup_files/install_miniforge.sh
+bash /scratch1/projects/scc_gwdg_danuma_ss2024/DaNuMa2024_setup/setup_files/install_miniforge.sh
 
 # run new bash
 source $HOME/.bashrc
 
-# # environment setup (not enough disc space yet for Pigdetect) ALSO MIM MIGHT LEAD TO PROBLEMS SIMILAR AS PIP WHICH MIGHT NOT BE EASILY FIXABLE FOR AUTOMATIC INSTALLATION
+# # environment setup
 source setup/setup.sh
-# cd ../PigDetect
-# source $HOME/repos/DaNuMa2024_setup/setup_files/PigDetect_setup.sh
+cd ../PigDetect
+source /scratch1/projects/scc_gwdg_danuma_ss2024/DaNuMa2024_setup/setup_files/PigDetect_setup.sh
 
 # install kernel for notebook
 python -m ipykernel install --user --name danuma --display-name "python danuma"
